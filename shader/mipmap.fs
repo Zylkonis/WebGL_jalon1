3aad8ecbc09fdfed89646931eedc735f28bda34f
Toriminos <titi.mimi12334567890@gmail.com>
2025-12-02 15:19:11 +0100

    test: Téo srx pour deux ligne de code

--- shader/mipmap.fs
+++ shader/mipmap.fs
@@ -17,16 +17,20 @@ void main(void) {
     float rgb_2 = texture2D(u_texture_grayscale_2, texCoords).r;
     float noiseBrightness = texture2D(u_noise, texCoords).r;
 
-    if(noiseBrightness < 0.4){
+    if(noiseBrightness < 0.2){
         gl_FragColor = vec4(texture2D(u_texture_2, texCoords).rgb, 1.0);
     }
-    else if(noiseBrightness > 0.6){
+    else if(noiseBrightness > 0.3){
         gl_FragColor = vec4(texture2D(u_texture_1, texCoords).rgb, 1.0);
     }
     else{
-        if(rgb_1 < rgb_2)
+        if(rgb_1 + noiseBrightness < rgb_2 + 1.0 - noiseBrightness)
             gl_FragColor = vec4(texture2D(u_texture_2, texCoords).rgb, 1.0);
         else
             gl_FragColor = vec4(texture2D(u_texture_1, texCoords).rgb, 1.0);
+
+        //float blend = vHeight / 0.2;
+        //finalColor = mix(texture2D(u_texture_2, texCoords).rgb, texture2D(u_texture_1, texCoords).rgb, blend);
+        //gl_FragColor = vec4(finalColor, 1.0);
     }
 }
