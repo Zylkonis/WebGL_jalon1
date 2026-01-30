20f99abebd80f290032b6c52d9b6f1073a2edb68
Remy <remy.martin@etu.univ-poitier.fr>
2026-01-30 10:57:17 +0100

    feat : mission secondaire je peut modifier la couleur du ciel

--- interface.js
+++ interface.js
@@ -1,3 +1,8 @@
+function SkyChangeColor(value){
+    const skyColor = hexToNormalizedRGB(value);
+    gl.clearColor(skyColor[0], skyColor[1], skyColor[2], 1.0);
+}
+
 function objFilePicker(file) {
     OBJ1.objName = URL.createObjectURL(file);
     OBJ1.Init();
